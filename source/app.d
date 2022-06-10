1b1c44696f662418510959ea8594f6226d80f5d8
Ikey Doherty <ikey@serpentos.com>
2022-06-10 22:31:22 +0100

    app: Add host config checking
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/app.d
+++ source/app.d
@@ -8,7 +8,73 @@
  * Authors: © 2022 Serpent OS Developers
  * License: ZLib
  */
+module main;
+
+import std.experimental.logger;
+
+/**
+ * BuildHost helps identify certain host OS requirements
+ */
+struct BuildHost
+{
+    /** True if /usr and / are merged */
+    bool usrMerged = true;
+}
+
+/**
+ * Known system configuration
+ */
+struct BuildConfiguration
+{
+    /** Information on the host */
+    BuildHost host;
+}
+
+/**
+ * Return a build configuration at runtime
+ *
+ * Returns: an instantiated BuildConfiguration.
+ */
+BuildConfiguration buildConfiguration()
+{
+    BuildConfiguration bc;
+    import std.file : isSymlink;
+    import std.file : readLink;
+    import std.path : absolutePath;
+
+    static struct CheckPath
+    {
+        string source;
+        string target;
+    }
+
+    CheckPath[] paths = [
+        CheckPath("/bin", "/usr/bin"), CheckPath("/sbin", "/usr/sbin"),
+    ];
+
+    foreach (p; paths)
+    {
+        if (isSymlink(p.source) && readLink(p.source).absolutePath("/") == p.target)
+        {
+            continue;
+        }
+        error(readLink(p.source).absolutePath);
+        bc.host.usrMerged = false;
+        break;
+    }
+
+    return bc;
+}
 
 void main()
 {
+    info("Checking host configuration");
+    const auto bc = buildConfiguration();
+    if (!bc.host.usrMerged)
+    {
+        fatal("Unsupported build host - /usr is not merged");
+        return;
+    }
+
+    info("Host configuration supported");
 }
