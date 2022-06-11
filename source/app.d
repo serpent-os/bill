350f86b141df0780917bc76415fba09a40381c55
Ikey Doherty <ikey@serpentos.com>
2022-06-11 11:29:34 +0100

    Split build configuration bits out
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/app.d
+++ source/app.d
@@ -10,7 +10,7 @@
  */
 module main;
 
-import std.stdio : stderr;
+import bill.buildconf;
 import bill.logging;
 
 /**
@@ -22,59 +22,6 @@ private static void configureLogging(LogLevel level = LogLevel.all)
     mlog.insertLogger("tui", new ColorLogger(level));
     mlog.insertLogger("file", new FileLogger("bill.log", level));
     sharedLog = mlog;
-}
-
-/**
- * BuildHost helps identify certain host OS requirements
- */
-struct BuildHost
-{
-    /** True if /usr and / are merged */
-    bool usrMerged = true;
-}
-
-/**
- * Known system configuration
- */
-struct BuildConfiguration
-{
-    /** Information on the host */
-    BuildHost host;
-}
-
-/**
- * Return a build configuration at runtime
- *
- * Returns: an instantiated BuildConfiguration.
- */
-BuildConfiguration buildConfiguration()
-{
-    BuildConfiguration bc;
-    import std.file : isSymlink;
-    import std.file : readLink;
-    import std.path : absolutePath;
-
-    static struct CheckPath
-    {
-        string source;
-        string target;
-    }
-
-    CheckPath[] paths = [
-        CheckPath("/bin", "/usr/bin"), CheckPath("/sbin", "/usr/sbin"),
-    ];
-
-    foreach (p; paths)
-    {
-        if (isSymlink(p.source) && readLink(p.source).absolutePath("/") == p.target)
-        {
-            continue;
-        }
-        bc.host.usrMerged = false;
-        break;
-    }
-
-    return bc;
 }
 
 void main()
---
+++ source/bill/buildconf.d
@@ -0,0 +1,65 @@
+/* SPDX-License-Identifier: Zlib */
+
+/**
+ * Build conf
+ *
+ * Helpers for determining build configuration
+ *
+ * Authors: © 2022 Serpent OS Developers
+ * License: ZLib
+ */
+module bill.buildconf;
+
+import std.file : isSymlink;
+import std.file : readLink;
+import std.path : absolutePath;
+
+/**
+ * BuildHost helps identify certain host OS requirements
+ */
+struct BuildHost
+{
+    /** True if /usr and / are merged */
+    bool usrMerged = true;
+}
+
+/**
+ * Known system configuration
+ */
+struct BuildConfiguration
+{
+    /** Information on the host */
+    BuildHost host;
+}
+
+/**
+ * Return a build configuration at runtime
+ *
+ * Returns: an instantiated BuildConfiguration.
+ */
+BuildConfiguration buildConfiguration()
+{
+    BuildConfiguration bc;
+
+    static struct CheckPath
+    {
+        string source;
+        string target;
+    }
+
+    CheckPath[] paths = [
+        CheckPath("/bin", "/usr/bin"), CheckPath("/sbin", "/usr/sbin"),
+    ];
+
+    foreach (p; paths)
+    {
+        if (isSymlink(p.source) && readLink(p.source).absolutePath("/") == p.target)
+        {
+            continue;
+        }
+        bc.host.usrMerged = false;
+        break;
+    }
+
+    return bc;
+}
