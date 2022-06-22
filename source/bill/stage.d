eec968234bbd15c782c3871dfa5d7833ef6cfa87
Ikey Doherty <ikey@serpentos.com>
2022-06-22 19:15:14 +0100

    bill: Add stage encapsulation
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/bill/stage.d
@@ -0,0 +1,52 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * Stage management
+ *
+ * Each stage in the bootstrap requires special management and
+ * an encapsulation of build tasks to ensure a sane bootstrap
+ * process.
+ *
+ * Authors: © 2022 Serpent OS Developers
+ * License: ZLib
+ */
+module bill.stage;
+
+/**
+ * Stage encapsulation
+ *
+ * Wraps contextual management and queue ordering.
+ */
+final class Stage
+{
+
+    @disable this();
+
+    /**
+     * Index property
+     *
+     * Returns: index of this stage
+     */
+    pure @property ulong index() @safe @nogc nothrow
+    {
+        return _index;
+    }
+
+package:
+
+    /**
+     * Construct stage with given index
+     */
+    this(ulong index)
+    {
+        _index = index;
+    }
+
+private:
+
+    ulong _index;
+}
