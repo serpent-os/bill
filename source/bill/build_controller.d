cbb98f53219ac989b316389c9a5ae6fa75d25fc1
Ikey Doherty <ikey@serpentos.com>
2022-06-27 10:13:29 +0100

    Add skeleton for BuildController
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/bill/build_controller.d
@@ -0,0 +1,24 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * bill.build_controller
+ *
+ * Manage the builds for a given set of input YAMLS
+ *
+ * Authors: Copyright © 2022 Serpent OS Developers
+ * License: Zlib
+ */
+
+module bill.build_controller;
+
+/** 
+ * Management of Build control
+ */
+final class BuildController
+{
+
+}
