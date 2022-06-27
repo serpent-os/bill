72080e005c26255a007e7919ba67fe55685c9869
Ikey Doherty <ikey@serpentos.com>
2022-06-27 11:22:46 +0100

    build_controller: Add no-op plugin
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/bill/build_controller.d
+++ source/bill/build_controller.d
@@ -18,23 +18,96 @@ module bill.build_controller;
 import moss.format.source.spec;
 import std.experimental.logger;
 import std.string : format;
+import moss.deps.registry;
+
+/**
+ * Handle integration of recipes in the Registry
+ */
+final class BuildPlugin : RegistryPlugin
+{
+
+    /**
+     * No support for anything
+     */
+    override RegistryItem[] queryProviders(in DependencyType dt, in string matcher,
+            ItemFlags flags = ItemFlags.None)
+    {
+        return null;
+    }
+
+    /** Noop */
+    override ItemInfo info(in string pkgID) const
+    {
+        return ItemInfo();
+    }
+
+    /** Noop */
+    override const(Dependency)[] dependencies(in string pkgID) const
+    {
+        return null;
+    }
+
+    /** Noop */
+    override const(Provider)[] providers(in string pkgID) const
+    {
+        return null;
+    }
+
+    /** noop */
+    override void fetchItem(FetchContext fc, in string pkgID)
+    {
+    }
+
+    /** Noop */
+    override const(RegistryItem)[] list(in ItemFlags flags) const
+    {
+        return null;
+    }
+
+    override NullableRegistryItem queryID(in string pkgID) const
+    {
+        return NullableRegistryItem(RegistryItem.init);
+    }
+
+    /** Noop */
+    override void close()
+    {
+    }
+
+    void addRecipe(Spec* recipe) @system
+    {
+        recipes ~= recipe;
+    }
+
+private:
+    Spec*[] recipes;
+
+}
 
 /** 
  * Management of Build control
  */
 final class BuildController
 {
+    this()
+    {
+        registry = new RegistryManager();
+        plugin = new BuildPlugin();
+        registry.addPlugin(plugin);
+    }
+
     /**
      * Add a recipe to our set of work
      */
     void addRecipe(Spec* recipe) @system
     {
-        recipes ~= recipe;
+        plugin.addRecipe(recipe);
         trace(format!"Loaded recipe for %s [%s]"(recipe.source.name,
                 recipe.source.versionIdentifier));
     }
 
 private:
 
-    Spec*[] recipes;
+    RegistryManager registry;
+    BuildPlugin plugin;
 }
